b76f8b582032f3f5fec826c6009862207f500618
ROBIN THOMAS <robin.19thomas@gmail.com>
2024-03-05 16:38:42 +0100

    Question 6

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import { readFileSync } from "fs";
 
 interface Person {
   age: number;
-  height: number;
+  height?: number;
+  taille?: number;
+
 }
 
 interface Statistics {
@@ -11,9 +13,11 @@ interface Statistics {
 }
 
 function getStatistics(): Statistics {
-  const persons: Person[] = JSON.parse(readFileSync("./persons.json").toString());
-  const totalAge = persons.reduce((total, person) => total + person.age, 0);
-  const totalHeight = persons.reduce((total, person) => total + person.height, 0);
+  const persons: Partial<Person>[] = JSON.parse(readFileSync("./persons.json").toString());
+  
+  const totalAge = persons.reduce((total, person) => total + (person.age as number), 0);
+  const totalHeight = persons.reduce((total, person) => total + (person.height || person.taille || 0), 0);
+
   const meanAge = totalAge / persons.length;
   const meanHeight = totalHeight / persons.length;
 
