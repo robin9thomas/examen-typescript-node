7d58cd5f391f582873d20f4b3554f0b622ea403f
ROBIN THOMAS <robin.19thomas@gmail.com>
2024-03-05 16:24:28 +0100

    Question 5

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,15 @@ interface Statistics {
 
 function getStatistics(): Statistics {
   const persons: Person[] = JSON.parse(readFileSync("./persons.json").toString());
-  const ages = persons.map(person => person.age);
-  return Math.max(...ages);
+  const totalAge = persons.reduce((total, person) => total + person.age, 0);
+  const totalHeight = persons.reduce((total, person) => total + person.height, 0);
+  const meanAge = totalAge / persons.length;
+  const meanHeight = totalHeight / persons.length;
+
+  return {
+    meanAge,
+    meanHeight
+  };
 }
 
 function displayResult() {
