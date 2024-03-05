7df5dcf4ccdd385db72cb67aa2ea80a72fdf3713
ROBIN THOMAS <robin.19thomas@gmail.com>
2024-03-05 16:18:54 +0100

    Question 2

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { readFileSync } from "fs";
 
+interface Person {
+  age: number;
+  height: number;
+}
+
 function getStatistics():number {
-  const persons = JSON.parse(readFileSync("./persons.json").toString());
+  const persons:Person[] = JSON.parse(readFileSync("./persons.json").toString());
 }
 
 function displayResult() {
