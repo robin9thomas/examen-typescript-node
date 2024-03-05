b0cbcae4858fd6b0b3d0a903d8fc98686cb17394
ROBIN THOMAS <robin.19thomas@gmail.com>
2024-03-05 16:44:51 +0100

    Question 7

--- src/index.ts
+++ src/index.ts
@@ -12,19 +12,31 @@ interface Statistics {
   meanHeight: number;
 }
 
-function getStatistics(): Statistics {
-  const persons: Partial<Person>[] = JSON.parse(readFileSync("./persons.json").toString());
-  
-  const totalAge = persons.reduce((total, person) => total + (person.age as number), 0);
-  const totalHeight = persons.reduce((total, person) => total + (person.height || person.taille || 0), 0);
+function getStatistics(): Statistics | null {
+  try {
+    const persons: Partial<Person>[] = JSON.parse(readFileSync("./persons.json").toString());
 
-  const meanAge = totalAge / persons.length;
-  const meanHeight = totalHeight / persons.length;
+    for (const person of persons) {
+      if (typeof person.age !== 'number' || (typeof person.height !== 'number' && typeof person.taille !== 'number')) {
+        console.error("Erreur : les données du fichier persons.json ne sont pas correctement formées.");
+        return null;
+      }
+    }
 
-  return {
-    meanAge,
-    meanHeight
-  };
+    const totalAge = persons.reduce((total, person) => total + (person.age || 0), 0);
+    const totalHeight = persons.reduce((total, person) => total + (person.height || person.taille || 0), 0);
+
+    const meanAge = totalAge / persons.length;
+    const meanHeight = totalHeight / persons.length;
+
+    return {
+      meanAge,
+      meanHeight
+    };
+  } catch (error) {
+    console.error("Erreur lors de l'analyse des données du fichier persons.json :", error);
+    return null;
+  }
 }
 
 function displayResult() {
